b0d5ce1e022bc70af471fae4a690a7ca697d4ee8
Himanshu Joshi <hiimanshuhh16@gmail.com>
2024-09-20 20:40:30 +0530

    adding files

--- src/Panel.jsx
+++ src/Panel.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Panel = () => {
+  const [color, setColor] = useState('olive')
   return (
     <>
-    <div className='hi'>
+    <div className='w-full h-screen duration-200' style={{backgroundColor:color}}>
       <div className=" flex justify-evenly text-white ">
-        <button className=' bg-slate-500 p-3 rounded-lg' onClick={changColor(white)}>white </button>
-        <button>Green </button>
-        <button>Blue </button>
-        <button>Brown </button>
-        <button>Pink </button>
+        <button className='  bg-black p-3 rounded-lg' onClick={()=> setColor('black')}>Black </button>
+        <button className=' bg-green-500 p-3 rounded-lg' onClick={()=> setColor('green')}>Green </button>
+        <button className=' bg-blue-500 p-3 rounded-lg' onClick={()=> setColor('blue')}>Blue </button>
+        <button className=' bg-red-500 p-3 rounded-lg' onClick={()=> setColor('red')}>red </button>
+        <button className=' bg-pink-500 p-3 rounded-lg' onClick={()=> setColor('pink')}>Pink </button>
       </div>
     </div>
     </>
-  )
+  ) 
 }
 
 export default Panel
